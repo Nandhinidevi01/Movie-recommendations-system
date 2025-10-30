397707323c73527ce9ecf73890bc197e90e27f22
selvendrantrs <160876110+selvendrantrs@users.noreply.github.com>
2025-10-30 17:23:10 +0530

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -43,3 +43,13 @@ for movie in sorted_similar_movies:
   if (i<30):
     print(i, '.',title_from_index)
     i+=1
+if 'sorted_similar_movies' in locals():
+    i = 1
+    for movie in sorted_similar_movies:
+        index = movie[0]
+        title_from_index = movies_data[movies_data.index==index]['title'].values[0]
+        if i < 30:
+            print(i, '.', title_from_index)
+            i += 1
+else:
+    print("No similar movies found. Make sure you ran the previous cells correctly.")
